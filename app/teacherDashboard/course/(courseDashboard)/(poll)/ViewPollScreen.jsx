455bd608e3ca43cde4dccf8bab8e6bc4bdcc786e
Eesha Patel <eesha.patel07@gmail.com>
2025-12-01 09:57:43 -0500

    Added CoursePollScreen, PollCard, ViewPollScreen

---
+++ app/teacherDashboard/course/(courseDashboard)/(poll)/ViewPollScreen.jsx
@@ -0,0 +1,64 @@
+import { useLocalSearchParams } from "expo-router";
+import { FlatList, Platform, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import MyButton2 from "../../../../../components/MyButton2";
+
+export default function ViewPollScreen() {
+    const { poll } = useLocalSearchParams();
+    const pollObj = JSON.parse(poll);
+
+    const API_URL =
+        Platform.OS === 'web'
+            ? process.env.EXPO_PUBLIC_API_URL_WEB
+            : process.env.EXPO_PUBLIC_API_URL_MOBILE;
+
+    const handleClosePoll = async () => {
+        try {
+            await fetch(`${API_URL}/api/polls/closePoll/${poll._id}`)
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
+        <SafeAreaView style={styles.screenContainer}>
+            {poll.isClosed ? (
+                <Text>
+                    Poll Status: Closed
+                </Text>
+            ) : (
+                <Text>
+                    Poll Status: Open
+                </Text>
+            )}
+
+            <FlatList
+                data={poll.options}
+                keyExtractor={item => item._id}
+                renderItem={({ item }) =>
+                    <>
+                        <Text>
+                            {item.text}
+                        </Text>
+                        <Text>
+                            {item.numVotes}
+                        </Text>
+                    </>
+                }
+            />
+
+            <MyButton2 onPress={handleClosePoll}>
+                <Text>
+                    Close Poll
+                </Text>
+            </MyButton2>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    screenContainer: {
+        flex: 1,
+    },
+
+})
---
+++ components/PollCard.jsx
@@ -0,0 +1,44 @@
+import { useRouter } from "expo-router";
+import { Pressable, StyleSheet } from "react-native";
+
+export default function PollCard({ poll }) {
+    const router = useRouter();
+
+    const handlePress = () => {
+        router.push({
+            pathname: "teacherDashboard/course/ViewPollScreen",
+            params: { poll: JSON.stringify(poll) }
+        });
+    }
+    return (
+        <Pressable onPress={handlePress} style={[styles.cardContainer, poll.isClosed ? styles.closedPoll : styles.openPoll]}>
+            <Text style={styles.title}>
+                {poll.title}
+                Ongoing poll since: {poll.dateCreated}
+            </Text>
+            {poll.expirationDate && (
+                <Text>
+                    Time left to vote:
+                </Text>
+            )}
+            <Text>
+                Checkbox to indicate if user already voted in the poll
+            </Text>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    cardContainer: {
+        borderWidth: 1,
+        height: 100,
+        width: 300,
+        padding: 10,
+    },
+    closedPoll: {
+        borderColor: "red"
+    },
+    openPoll: {
+        borderColor: "green"
+    }
+})
