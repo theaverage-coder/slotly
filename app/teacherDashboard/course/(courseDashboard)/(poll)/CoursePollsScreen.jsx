908a0c8155e4f95219c69c3cb5ae9f28984f9fc7
Eesha Patel <eesha.patel07@gmail.com>
2025-12-01 09:57:43 -0500

    Added CoursePollScreen, PollCard, ViewPollScreen

---
+++ app/teacherDashboard/course/(courseDashboard)/(poll)/CoursePollsScreen.jsx
@@ -0,0 +1,74 @@
+import { useFocusEffect, useRouter } from "expo-router";
+import { useCallback } from "react";
+import { FlatList, Platform, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import MyButton2 from "../../../../../components/MyButton2";
+
+export default function CoursePollsScreen() {
+    const [polls, setPolls] = useState([]);
+    const router = useRouter();
+
+    const API_URL =
+        Platform.OS === 'web'
+            ? process.env.EXPO_PUBLIC_API_URL_WEB
+            : process.env.EXPO_PUBLIC_API_URL_MOBILE;
+
+    useFocusEffect(useCallback(() => {
+        const fetchPolls = async () => {
+            try {
+                const response = await fetch(`${API_URL}/api/polls/getAllPolls/${courseId}`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setPolls(data);
+                }
+            } catch (err) {
+                console.log("Failed to retrieve polls: ", err);
+            }
+        }
+
+        fetchPolls();
+    }, [])
+    );
+
+    return (
+        <SafeAreaView style={styles.screenContainer}>
+            <View style={{ flex: 1 }}>
+                {polls.length === 0 ? (
+                    <View style={styles.noPollsTextContainer}>
+                        <Text style={styles.noPollsText}>
+                            No ongoing polls
+                        </Text>
+                    </View>
+                ) : (
+                    <FlatList
+                        style={styles.pollsList}
+                        contentContainerStyle={{ gap: 20, alignItems: "center" }}
+                        data={polls}
+                        keyExtractor={item => item._id}
+                        renderItem={({ item }) => <PollCard poll={item} />}
+                    />
+                )}
+            </View>
+
+            <MyButton2 onPress={() => router.navigate("teacherDashboard/course/CreatePollScreenOne")} style={{ backgroundColor: "rgba(217, 217, 217, 1)", textColor: "rgba(33, 33, 33, 1)" }}>
+                <Text> Create Poll </Text>
+            </MyButton2>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    screenContainer: {
+        flex: 1,
+        backgroundColor: "rgba(33, 33, 33, 1)",
+    },
+    noEventsTextContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1
+    },
+    noEventsText: {
+        fontSize: 20,
+        color: "white"
+    },
+})
