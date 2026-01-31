6ddbc64086130f0a1432886eb2b28e0c27c86584
Eesha Patel <eesha.patel07@gmail.com>
2026-01-31 13:42:56 -0500

    Fixed bookAppointment API
    Fixed bugs and return type of getAvailableTimeSlots API
    
    Modified BookAppointmentScreen UI
    
    Added message field to Appointment model
    
    Navigate back after booking appointment

--- app/studentDashboard/course/(courseDashboard)/(detailsAndBookings)/BookAppointmentScreen.jsx
+++ app/studentDashboard/course/(courseDashboard)/(detailsAndBookings)/BookAppointmentScreen.jsx
@@ -1,7 +1,7 @@
-import { useFocusEffect } from "expo-router";
+import { useFocusEffect, useRouter } from "expo-router";
 import { useCallback, useState } from "react";
-import { FlatList, Platform, Pressable, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList, Keyboard, Platform, Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import MyButton2 from "../../../../../components/MyButton2";
 import { useCourseContext } from "../../../../../contexts/CourseContext";
 import { useUser } from "../../../../../contexts/UserContext";
 
@@ -14,6 +14,8 @@ export default function BookAppointmentScreen() {
     const { user } = useUser();
     const [bookingId, setBookingId] = useState(null);
     const [timeSlotDuration, setTimeSlotDuration] = useState(null);
+    const [message, setMessage] = useState("");
+    const router = useRouter();
 
     const API_URL =
         Platform.OS === 'web'
@@ -22,7 +24,6 @@ export default function BookAppointmentScreen() {
 
 
     const fetchAvailableTimeslots = async () => {
-        //console.log("calling once")
         try {
             const response = await fetch(`${API_URL}/api/bookings/getAvailableTimeSlots/${courseId}/${startDate}`)
             const data = await response.json();
@@ -37,7 +38,6 @@ export default function BookAppointmentScreen() {
 
     useFocusEffect(useCallback(() => {
         fetchAvailableTimeslots();
-        //console.log("usefocuseffect")
     }, [])
     );
 
@@ -48,54 +48,86 @@ export default function BookAppointmentScreen() {
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
                     selectedTimeSlot: selectedTimeSlot,
-                    selectedDate: selectedDate,
                     bookingId: bookingId,
                     studentId: user._id,
-                    timeSlotDuration: timeSlotDuration
+                    timeSlotDuration: timeSlotDuration,
+                    message: message
                 })
             });
 
             if (response.ok) {
                 console.log("Appointment booked!");
+                router.back();
+            } else {
+                console.log("Failed to book appointment");
             }
 
         } catch (err) {
             console.log(err);
         }
     }
+
+    const getMonthAndDay = (str) => {
+        return str.split("-");
+    }
+
+    const getTimeString = (date) => {
+        return new Date(date).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+    }
+
     return (
-        <SafeAreaView>
+        <Pressable style={styles.screenContainer} onPress={Keyboard.dismiss}>
             {(!availableSlots) ? (
                 <Text> No available time slots</Text>
             ) : (
-                <View>
-                    <FlatList
-                        data={Object.keys(availableSlots)}
-                        horizontal={true}
-                        keyExtractor={item => item}
-                        //onEndReachedThreshold={0.9}
-                        //onEndReached={fetchAvailableTimeslots()}
-                        renderItem={({ item }) =>
-                            <Pressable onPress={() => setSelectedDate(item)}>
-                                <Text>
-                                    {item}
-                                </Text>
-                            </Pressable>
-                        }
-                    >
-                    </FlatList>
-                </View>
+                <>
+                    <View style={styles.title}>
+                        <Text style={styles.titleText}> Pick a Date and Time </Text>
+                    </View>
+                    <View>
+                        <FlatList
+                            data={Object.keys(availableSlots)}
+                            horizontal={true}
+                            keyExtractor={item => item}
+                            //onEndReachedThreshold={0.9}
+                            //onEndReached={fetchAvailableTimeslots()}
+                            renderItem={({ item }) => {
+                                const monthAndDay = getMonthAndDay(item);
+                                return (
+                                    <Pressable
+                                        style={[styles.dateCard, availableSlots[item].length !== 0 && styles.availableDate, selectedDate === item && styles.selectedDate]}
+                                        onPress={() => setSelectedDate(item)}>
+                                        <Text style={styles.dayOfWeekText}>
+                                            {monthAndDay[0]}
+                                        </Text>
+                                        <Text>
+                                            {monthAndDay[1]} {monthAndDay[2]}
+                                        </Text>
+                                    </Pressable>
+                                )
+                            }}
+                        >
+                        </FlatList>
+                    </View>
+                </>
             )}
             {(!selectedDate) ? (
                 <Text> Select a date to view available times </Text>
             ) : (
-                <View>
+                <View style={styles.timeSlotsContainer}>
+                    <Text> Available Times </Text>
                     <FlatList
                         data={availableSlots[selectedDate]}
                         keyExtractor={item => item}
+                        numColumns={4}
+                        contentContainerStyle={{
+                            alignItems: 'center',
+                        }}
                         renderItem={({ item }) =>
-                            <Pressable onPress={() => setSelectedTimeSlot}>
-                                <Text> {item} </Text>
+                            <Pressable
+                                style={[styles.timeSlotCard, item === selectedTimeSlot && styles.selectedTimeSlot]}
+                                onPress={() => setSelectedTimeSlot(item)}>
+                                <Text> {getTimeString(item)} </Text>
                             </Pressable>
                         }
                     >
@@ -105,10 +137,90 @@ export default function BookAppointmentScreen() {
             {(!selectedTimeSlot) ? (
                 <></>
             ) : (
-                <Pressable onPress={handleBookAppointment}>
-                    <Text> Make an appointment</Text>
-                </Pressable>
+                <>
+                    <Text> selected date: {selectedDate} ; selected time slot: {new Date(selectedTimeSlot).toLocaleString()} </Text>
+                    <View style={styles.messageContainer}>
+                        <TextInput
+                            style={styles.textField}
+                            multiline={true}
+                            placeholder="Add a message for your professor (ie. what the meeting is about)"
+                            value={message}
+                            onChangeText={(text) => setMessage(text)}
+                        />
+                    </View>
+
+                    <MyButton2 onPress={handleBookAppointment} style={{ backgroundColor: "rgba(217, 217, 217, 1)", textColor: "rgba(33, 33, 33, 1)", borderRadius: 10 }}>
+                        <Text> Make an appointment</Text>
+                    </MyButton2>
+                </>
             )}
-        </SafeAreaView>
+        </Pressable>
     );
 }
+
+const styles = StyleSheet.create({
+    screenContainer: {
+        flex: 1,
+        backgroundColor: "rgba(33, 33, 33, 1)",
+    },
+    title: {
+        marginVertical: 20,
+        paddingLeft: 15
+    },
+    titleText: {
+        color: "white",
+        fontSize: 25,
+        fontWeight: "bold",
+    },
+    dateCard: {
+        marginHorizontal: 7,
+        height: 80,
+        width: 60,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "rgba(88, 85, 85, 0.2)",
+        borderRadius: 10,
+    },
+    availableDate: {
+        backgroundColor: "rgb(88, 85, 85)"
+    },
+    selectedDate: {
+        borderWidth: 3,
+        borderColor: "rgb(125, 78, 87)"
+    },
+    dayOfWeekText: {
+        fontWeight: "bold",
+        fontSize: 18,
+        marginBottom: 5
+    },
+    timeSlotsContainer: {
+        marginVertical: 30,
+        flex: 1
+    },
+    timeSlotCard: {
+        width: 70,
+        height: 40,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 10,
+        marginHorizontal: 7,
+        marginVertical: 10,
+        backgroundColor: "rgb(88, 85, 85)"
+    },
+    selectedTimeSlot: {
+        backgroundColor: "rgb(165, 104, 115)",
+        borderWidth: 0,
+    },
+    textField: {
+        width: "95%",
+        height: 130,
+        backgroundColor: "rgba(50, 50, 50, 1)",
+        //justifyContent: "center",
+        borderRadius: 16,
+        padding: 20,
+        color: "rgba(255, 255, 255, 1)",
+    },
+    messageContainer: {
+        alignItems: "center",
+    }
+})
