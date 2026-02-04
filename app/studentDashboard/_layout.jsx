2a3de0b5a48c3dd944b01cce5995cabf213941df
Eesha Patel <eesha.patel07@gmail.com>
2026-02-04 09:42:28 -0500

    Added vector icons to tab bar and ViewEventScreen

--- app/studentDashboard/_layout.jsx
+++ app/studentDashboard/_layout.jsx
@@ -1,3 +1,4 @@
+import Ionicons from "@react-native-vector-icons/ionicons";
 import { Tabs } from "expo-router";
 
 export default function StudentLayout() {
@@ -18,9 +19,24 @@ export default function StudentLayout() {
                 tabBarInactiveTintColor: "grey",
             }}>
 
-            <Tabs.Screen name="(home)" options={{ title: 'Meetings' }} />
-            <Tabs.Screen name="course" options={{ title: 'Courses' }} />
-            <Tabs.Screen name="ProfileScreen" options={{ title: 'Profile' }} />
+            <Tabs.Screen name="(home)" options={{
+                title: 'Meetings',
+                tabBarIcon: ({ focused }) => (
+                    <Ionicons size={24} color="white" name={focused ? "reader" : "reader-outline"} />
+                )
+            }} />
+            <Tabs.Screen name="course" options={{
+                title: 'Courses',
+                tabBarIcon: ({ focused }) => (
+                    <Ionicons size={24} color="white" name={focused ? "school" : "school-outline"} />
+                )
+            }} />
+            <Tabs.Screen name="ProfileScreen" options={{
+                title: 'Profile',
+                tabBarIcon: ({ focused }) => (
+                    <Ionicons size={24} color="white" name={focused ? "person" : "person-outline"} />
+                )
+            }} />
         </Tabs>
     );
 }
