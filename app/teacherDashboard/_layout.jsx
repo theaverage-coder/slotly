d83c90e2e4dd33734264b35397f27677b2b63efc
Eesha Patel <eesha.patel07@gmail.com>
2026-02-04 20:47:02 -0500

    Added getAllStudents API, deleteCourse API and editCourse API
    Modified teacher dashboard's bottom bar UI
    
    CourseDetailsScreen in teacherDashboard now has a modal that pulls up different content based on chosen feature

--- app/teacherDashboard/_layout.jsx
+++ app/teacherDashboard/_layout.jsx
@@ -3,23 +3,36 @@ import { Tabs } from "expo-router";
 
 export default function TeacherLayout() {
     return (
-        <Tabs screenOptions={{ headerShown: false }}>
+        <Tabs screenOptions={{
+            headerShown: false,
+            tabBarStyle: {
+                backgroundColor: "rgba(33, 33, 33, 1)",
+                paddingVertical: 10,
+                paddingHorizontal: 20,
+                color: "rgba(255, 255, 255, 1)",
+                fontSize: 12,
+                fontWeight: 400,
+                borderTopWidth: 0,
+            },
+            tabBarActiveTintColor: "white",
+            tabBarInactiveTintColor: "grey",
+        }}>
             <Tabs.Screen name="HomeScreen" options={{
                 title: 'Meetings',
                 tabBarIcon: ({ focused }) => (
-                    <Ionicons size={24} color="white" name={focused ? "reader" : "reader-outline"} />
+                    <Ionicons size={30} color="white" name={focused ? "reader" : "reader-outline"} />
                 )
             }} />
             <Tabs.Screen name="course" options={{
                 title: 'Courses',
                 tabBarIcon: ({ focused }) => (
-                    <Ionicons size={24} color="white" name={focused ? "school" : "school-outline"} />
+                    <Ionicons size={30} color="white" name={focused ? "school" : "school-outline"} />
                 )
             }} />
             <Tabs.Screen name="ProfileScreen" options={{
                 title: 'Profile',
                 tabBarIcon: ({ focused }) => (
-                    <Ionicons size={24} color="white" name={focused ? "person" : "person-outline"} />
+                    <Ionicons size={30} color="white" name={focused ? "person" : "person-outline"} />
                 )
             }} />
         </Tabs>
--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { addCourse, joinCourse, getCourses, getCourseById } = require('../controllers/courseController');
+const { addCourse, joinCourse, getCourses, getCourseById, getAllStudents, deleteCourse, editCourse } = require('../controllers/courseController');
 
 router.post('/addCourse', addCourse);
 router.post('/joinCourse', joinCourse);
 router.post('/getCourses', getCourses);
 router.get('/getCourseById/:courseId', getCourseById);
-
+router.get('/getAllStudents/:courseId', getAllStudents);
+router.delete("/deleteCourse/:courseId", deleteCourse);
+router.patch("/editCourse/:courseId", editCourse);
 module.exports = router
