4485a1e3aced3a0a94e4ddb78594753a1195ec30
Eesha Patel <eesha.patel07@gmail.com>
2026-02-14 20:39:49 -0500

    Added getCreatedEvents API, HomeScreen now displays a teacher's events as meetings
    Created shared component for HomeScreen

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { createEvent, joinEvent, getEvent, leaveEvent, getAllJoinedEvents, getAllEvents, deleteEvent, getStudents } = require('../controllers/eventController');
+const { createEvent, joinEvent, getEvent, leaveEvent, getAllJoinedEvents, getAllEvents, deleteEvent, getStudents, getCreatedEvents } = require('../controllers/eventController');
 const protect = require("../middleware/authMiddleware");
 
 router.post('/createEvent', protect, createEvent);
@@ -9,6 +9,8 @@ router.get("/getEvent", protect, getEvent);
 router.patch("/leaveEvent/:eventId", protect, leaveEvent);
 router.get("/getAllEvents/:courseId", getAllEvents);
 router.delete("/deleteEvent/:eventId", protect, deleteEvent);
-router.post('/getAllJoinedEvents', protect, getAllJoinedEvents);
-router.get("/getStudents/:eventId", protect, getStudents)
+router.get('/getAllJoinedEvents', protect, getAllJoinedEvents);
+router.get("/getStudents/:eventId", protect, getStudents);
+router.get("/getCreatedEvents/", getCreatedEvents);
+
 module.exports = router
